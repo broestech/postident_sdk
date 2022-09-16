0af7a7445f0ac9aa7a10a7cfbca93a554ca796d2
ViktorZieg <91725255+ViktorZieg@users.noreply.github.com>
2022-09-16 13:40:29 +0200

    added getSigningUri to PostIdentConfiguration

--- src/main/kotlin/com/broeskamp/postident/PostIdentConfiguration.kt
+++ src/main/kotlin/com/broeskamp/postident/PostIdentConfiguration.kt
@@ -3,19 +3,27 @@ package com.broeskamp.postident
 import java.util.*
 
 
-val CONTENT_TYPE_HEADER_NAME: String = "Content-Type"
-val CONTENT_TYPE_HEADER_VALUE: String = "application/json"
+class PostIdentConfiguration(
+    private var username: String,
+    private var password: String,
+    private var clientId: String,
+) {
 
-val AUTHORIZATION_HEADER_NAME: String = "Authorization"
+    val CONTENT_TYPE_HEADER_NAME: String = "Content-Type"
+    val CONTENT_TYPE_HEADER_VALUE: String = "application/json"
 
-val BASE_URL: String = "https://postident.deutschepost.de/api/"
-val SIGNING_PATH: String = "scr-signing/v2/"
+    val AUTHORIZATION_HEADER_NAME: String = "Authorization"
 
-
-class PostIdentConfiguration(private var username: String, private var password: String) {
+    private val BASE_URL: String = "https://postident.deutschepost.de/api/"
+    private val SIGNING_PATH: String = "scr-signing/v2/"
 
     fun getAuthValue(): String {
-        val authData = Base64.getEncoder().encodeToString("%s:%s".format(username, password).toByteArray())
-        return "Basic %s".format(authData);
+        val authData =
+            Base64.getEncoder().encodeToString("%s:%s".format(username, password).toByteArray())
+        return "Basic %s".format(authData)
+    }
+
+    fun getSigningUri(): String {
+        return "$BASE_URL$SIGNING_PATH$clientId/signingcases"
     }
 }
