2474e79a766ff4e7907c93661f2bdf2d66f5b01c
ViktorZieg <91725255+ViktorZieg@users.noreply.github.com>
2022-09-16 13:41:09 +0200

    added createSigningCase() to PostIdent

--- src/main/kotlin/com/broeskamp/postident/PostIdent.kt
+++ src/main/kotlin/com/broeskamp/postident/PostIdent.kt
@@ -2,13 +2,40 @@ package com.broeskamp.postident
 
 
 import com.broeskamp.postident.dto.request.SigningCaseRequest
+import com.broeskamp.postident.dto.response.SigningCaseResponse
 import com.fasterxml.jackson.databind.ObjectMapper
+import java.net.URI
 import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpRequest.BodyPublishers
+import java.net.http.HttpResponse.BodyHandlers
 
 class PostIdent(private val config: PostIdentConfiguration) {
 
     private val mapper: ObjectMapper = ObjectMapper()
     private val httpClient: HttpClient = HttpClient.newHttpClient()
 
-    fun createSigningCase(signingCaseRequest: SigningCaseRequest):
+    fun createSigningCase(signingCaseRequest: SigningCaseRequest): SigningCaseResponse {
+        val request = getHttpRequestBuilder()
+            .uri(URI.create(config.getSigningUri()))
+            .POST(BodyPublishers.ofString(mapper.writeValueAsString(signingCaseRequest)))
+            .build()
+        return executeRequest(request, SigningCaseResponse::class.java)
+    }
+
+    private fun <T> executeRequest(request: HttpRequest, responseClass: Class<T>): T {
+        try {
+            val response = httpClient.send(request, BodyHandlers.ofString())
+            return mapper.readValue(response.body(), responseClass)
+        } catch (exception: InterruptedException) {
+            Thread.currentThread().interrupt()
+            throw ThreadInterruptedException(exception)
+        }
+    }
+
+    private fun getHttpRequestBuilder(): HttpRequest.Builder {
+        return HttpRequest.newBuilder()
+            .header(config.CONTENT_TYPE_HEADER_NAME, config.CONTENT_TYPE_HEADER_VALUE)
+            .header(config.AUTHORIZATION_HEADER_NAME, config.getAuthValue())
+    }
 }
