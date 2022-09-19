09d86939175d6a270fabcba47aa811b4bcb5a62c
ViktorZieg <91725255+ViktorZieg@users.noreply.github.com>
2022-09-19 11:45:43 +0200

    added jackson, maven-publish and builder dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.7.10"
-    application
-}
 
-group = "com.broeskamp"
-version = "1.0-SNAPSHOT"
+plugins {
+    `maven-publish`
+    kotlin("jvm") version "1.7.10"
+    kotlin("kapt") version "1.7.10"
+}
 
 repositories {
     mavenCentral()
@@ -16,6 +15,9 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.4")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.4")
+    implementation("com.thinkinglogic.builder:kotlin-builder-annotation:1.2.1")
+    kapt("com.thinkinglogic.builder:kotlin-builder-processor:1.2.1")
 }
 
 tasks.test {
@@ -26,6 +28,13 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.broeskamp"
+            artifactId = "postident_sdk"
+            version = "1.0-SNAPSHOT"
+            from(components["java"])
+        }
+    }
 }
