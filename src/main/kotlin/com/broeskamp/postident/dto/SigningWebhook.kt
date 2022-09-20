9d5bb5a7534090f0f096b53e37c198f7ec90fc6a
ViktorZieg <91725255+ViktorZieg@users.noreply.github.com>
2022-09-20 16:15:38 +0200

    added SigningWebhook

---
+++ src/main/kotlin/com/broeskamp/postident/dto/SigningWebhook.kt
@@ -0,0 +1,9 @@
+package com.broeskamp.postident.dto
+
+import com.thinkinglogic.builder.annotation.Builder
+
+@Builder
+data class SigningWebhook(
+    val caseId: String,
+    val referenceId: String,
+)
