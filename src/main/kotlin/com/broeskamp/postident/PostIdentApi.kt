7fd53d150b043b2ec024dc2881f0319dbb2217dd
ViktorZieg <91725255+ViktorZieg@users.noreply.github.com>
2022-09-27 11:14:16 +0200

    added @JvmOverloads to constructor

--- src/main/kotlin/com/broeskamp/postident/PostIdentApi.kt
+++ src/main/kotlin/com/broeskamp/postident/PostIdentApi.kt
@@ -6,8 +6,9 @@ import com.broeskamp.postident.dto.response.SigningCaseResponse
 import com.broeskamp.postident.dto.result.ident.IdentCaseResult
 import com.broeskamp.postident.dto.result.sign.SigningCaseResult
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import java.net.URI
 import java.net.http.HttpRequest
 import java.net.http.HttpRequest.BodyPublishers
 import java.net.http.HttpResponse.BodyHandlers
@@ -18,9 +19,10 @@ private const val CONTENT_TYPE_HEADER_VALUE: String = "application/json"
 private const val AUTHORIZATION_HEADER_NAME: String = "Authorization"
 
 
-class PostIdentApi(
+class PostIdentApi @JvmOverloads constructor(
     private val config: PostIdentConfiguration,
-    private val mapper: ObjectMapper = jacksonObjectMapper()
+    private val mapper: ObjectMapper = jacksonObjectMapper().registerModule(JavaTimeModule())
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
 ) {
 
     fun createSigningCase(signingCaseRequest: SigningCaseRequest): CompletableFuture<SigningCaseResponse> {
@@ -31,7 +33,7 @@ class PostIdentApi(
         return executeRequest(request, SigningCaseResponse::class.java)
     }
 
-    fun retrieveSigningCaseResult(caseId: String): CompletableFuture<SigningCaseResult>{
+    fun retrieveSigningCaseResult(caseId: String): CompletableFuture<SigningCaseResult> {
         val request = getHttpRequestBuilder()
             .uri(config.getSigningResultUri(caseId))
             .GET()
@@ -39,7 +41,7 @@ class PostIdentApi(
         return executeRequest(request, SigningCaseResult::class.java)
     }
 
-    fun retrieveIdentCaseResult(caseId: String): CompletableFuture<IdentCaseResult>{
+    fun retrieveIdentCaseResult(caseId: String): CompletableFuture<IdentCaseResult> {
         val request = getHttpRequestBuilder()
             .uri(config.getIdentResultUri(caseId))
             .GET()
@@ -53,7 +55,7 @@ class PostIdentApi(
     ): CompletableFuture<T> {
         val futureResponse = config.httpClient.sendAsync(request, BodyHandlers.ofString())
         return futureResponse.thenApply { response ->
-            if (response.statusCode() == 200) {
+            if (response.statusCode() == 201) {
                 return@thenApply mapper.readValue(response.body(), responseClass)
             } else {
                 throw PostIdentApiException(request, response)
